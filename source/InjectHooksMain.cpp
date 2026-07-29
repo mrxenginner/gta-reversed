18585422f163bf8a29d926026522909e423b8a6b
Pirulax <patrikjankovics7@gmail.com>
2026-07-29 12:27:31 +0200

    re: `CTaskInteriorLieInBed` (#491)
    * `CPlaceable` Refactor `GetHeading`
    
    * CPedAcquaintanceScanner: `SetOnlyScriptPedAllowed` and `TurnOffAllScanners`
    
    * `CTaskInteriorLieInBed`
    
    * Fix missing transform
    
    * Fix code, add hook
    
    * Move to `/Interior`
    
    * Fix include
    
    * fix build after merge

--- source/InjectHooksMain.cpp
+++ source/InjectHooksMain.cpp
@@ -294,6 +294,7 @@
 #include "TaskSimpleGiveCPR.h"
 #include "TaskSimpleCarSetPedInAsPassenger.h"
 #include "TaskComplexDriveFireTruck.h"
+#include "Interior/TaskInteriorLieInBed.h"
 #include "TaskSimpleSwim.h"
 #include "TaskComplexWalkRoundObject.h"
 #include "TaskSimplePause.h"
@@ -955,7 +956,7 @@ void InjectHooksMain() {
             CTaskInteriorBeInOffice::InjectHooks();
             CTaskInteriorBeInShop::InjectHooks();
             CTaskInteriorGoToInfo::InjectHooks();
-        // CTaskInteriorLieInBed::InjectHooks();
+            CTaskInteriorLieInBed::InjectHooks();
             CTaskInteriorShopKeeper::InjectHooks();
             CTaskInteriorSitAtDesk::InjectHooks();
         // CTaskInteriorSitInChair::InjectHooks();
