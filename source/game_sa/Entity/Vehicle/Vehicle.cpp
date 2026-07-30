f5890d43f5741de9a1119050369832963900b546
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-07-30 09:25:23 +0100

    fix(CVehicle): Correct left-wheel result in FindTyreNearestPoint (#1424)
    * fix(CVehicle): Correct left-wheel result in FindTyreNearestPoint
    
    For a non-bike vehicle the front-left and rear-left results were swapped:
    the original at 0x6D7BC0 returns FRONT_LEFT when dot(GetRight) <= 0 and
    REAR_LEFT otherwise, the reversed code had them the other way around.
    
    The axes themselves match the original (forward for left/right, right for
    front/rear), so only the two left-wheel constants needed swapping.
    
    Fixes #1257
    
    * fix(CVehicle): return eNearestCarWheel from FindTyreNearestPoint

--- source/game_sa/Entity/Vehicle/Vehicle.cpp
+++ source/game_sa/Entity/Vehicle/Vehicle.cpp
@@ -3293,16 +3293,16 @@ void CVehicle::ProcessBikeWheel(CVector& wheelFwd, CVector& wheelRight, CVector&
 }
 
 // 0x6D7BC0
-eCarWheel CVehicle::FindTyreNearestPoint(CVector2D point) {
+auto CVehicle::FindTyreNearestPoint(CVector2D point) -> eNearestCarWheel {
     const auto relativePt = point - GetPosition2D();
-    const bool isRight = relativePt.Dot(GetForward()) <= 0.f; // TODO: This doesn't make a lot of sense, why is Y used for left/right?
-    if (IsBike()) {
-        return isRight ? CAR_WHEEL_FRONT_RIGHT : CAR_WHEEL_FRONT_LEFT;
+    const bool isFront = relativePt.Dot(GetForward()) > 0.f;
+    if (IsBike()) { // only distinguishes front vs rear
+        return isFront ? eNearestCarWheel::FRONT_LEFT : eNearestCarWheel::REAR_LEFT;
     }
-    const bool isFront = relativePt.Dot(GetRight()) <= 0.f; // TODO: Same here, why is X used for front/rear?
-    return isRight
-        ? isFront ? CAR_WHEEL_FRONT_RIGHT : CAR_WHEEL_REAR_RIGHT
-        : isFront ? CAR_WHEEL_REAR_LEFT : CAR_WHEEL_FRONT_LEFT;
+    const bool isRight = relativePt.Dot(GetRight()) > 0.f;
+    return isFront
+        ? isRight ? eNearestCarWheel::FRONT_RIGHT : eNearestCarWheel::FRONT_LEFT
+        : isRight ? eNearestCarWheel::REAR_RIGHT : eNearestCarWheel::REAR_LEFT;
 }
 
 // 0x6D7C90
